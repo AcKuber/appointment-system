8919528888eb8c29f8a4dd0b30b3f5dabba70ec0
AcKuber <acharyakuber188@gmail.com>
2022-05-15 10:56:13 +0545

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-Updated soon...
+<div id="top"></div>
+
+<br />
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+
+TThis is online appointment system which allows user to book appointment with any officer. It contains three entity Officer, Visitor and Activity.
+
+
+
+### Built With
+
+* [Laravel](https://laravel.com)
+* [TailWindCSS](https://tailwindcss.com/)
+* [JQuery](https://jquery.com)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+.
+
+### Prerequisites
+
+* 	Composer(https://getcomposer.org/)
+  ```
+
+### Installation
+
+1. Clone the repo
+   git clone https://github.com/AcKuber/appointment-system
+   ```
+2. Install composer dependencies
+   
+   composer install
+   ```
+3. Generate .env file
+
+   copy .env.example .env
+
+ 4. Generate application key
+
+ 	php artisan key:generate
+
+ 5. Open phpmyadmin or your favorite mysql databse acccess interface
+
+ 6. Create database (say online-appointment)
+
+ 7. Import sql file 'sql_dump.sql' [which can be found at (appointment-system/database/sql_dump.sql)]
+
+ 8. Change database name in .env file as you created [say DB_DATABASE=online_appointment]
+
+ 9. Run the project
+ 	php artisan serve 
+   ```
+
+<p align="right">(<a href="#top">back to top</a>)</p>
--- database/sql_dump.sql
+++ database/sql_dump.sql
@@ -1,27 +1,34 @@
-/*
-SQLyog Professional v13.1.1 (64 bit)
-MySQL - 10.4.22-MariaDB : Database - online_appointment
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2022 at 07:09 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`online_appointment` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `online_appointment`;
+--
+-- Database: `online_appointment`
+--
 
-/*Table structure for table `activity` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `activity`;
+--
+-- Table structure for table `activity`
+--
 
 CREATE TABLE `activity` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `aname` varchar(100) NOT NULL,
   `atype` varchar(20) DEFAULT NULL CHECK (`atype` in ('Leave','Appointment','Break')),
   `astatus` varchar(20) DEFAULT NULL CHECK (`astatus` in ('Active','Cancelled','Deactivated')),
@@ -30,95 +37,169 @@ CREATE TABLE `activity` (
   `endTime` time DEFAULT NULL,
   `addedOn` datetime DEFAULT current_timestamp(),
   `officer_id` int(11) NOT NULL,
-  `visitor_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `officer_id` (`officer_id`),
-  KEY `visitor_id` (`visitor_id`),
-  CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`officer_id`) REFERENCES `officer` (`id`),
-  CONSTRAINT `activity_ibfk_2` FOREIGN KEY (`visitor_id`) REFERENCES `visitor` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+  `visitor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `activity` */
+--
+-- Dumping data for table `activity`
+--
 
-insert  into `activity`(`id`,`aname`,`atype`,`astatus`,`adate`,`startTime`,`endTime`,`addedOn`,`officer_id`,`visitor_id`) values 
-(1,'Leave for festival','Leave','Active','2022-05-15 00:00:00','09:00:00','17:00:00','2022-05-11 12:18:49',11,NULL),
-(2,'Meeting with Manoj Sir','Appointment','Active','2022-05-18 00:00:00','14:17:00','15:18:00','2022-05-11 13:17:32',11,2),
-(4,'Leave for test user','Leave','Deactivated','2022-05-20 00:00:00','20:43:00','21:45:00','2022-05-15 07:43:22',10,NULL),
-(5,'Meeting with Manoj Sir','Appointment','Active','2022-05-27 00:00:00','10:43:00','11:45:00','2022-05-15 07:44:18',11,2);
+INSERT INTO `activity` (`id`, `aname`, `atype`, `astatus`, `adate`, `startTime`, `endTime`, `addedOn`, `officer_id`, `visitor_id`) VALUES
+(1, 'Leave for festival', 'Leave', 'Active', '2022-05-15 00:00:00', '09:00:00', '17:00:00', '2022-05-11 12:18:49', 11, NULL),
+(2, 'Meeting with Manoj Sir', 'Appointment', 'Active', '2022-05-18 00:00:00', '14:17:00', '15:18:00', '2022-05-11 13:17:32', 11, 2),
+(4, 'Leave for test user', 'Leave', 'Deactivated', '2022-05-20 00:00:00', '20:43:00', '21:45:00', '2022-05-15 07:43:22', 10, NULL),
+(5, 'Meeting with Manoj Sir', 'Appointment', 'Active', '2022-05-27 00:00:00', '10:43:00', '11:45:00', '2022-05-15 07:44:18', 11, 2);
 
-/*Table structure for table `officer` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `officer`;
+--
+-- Table structure for table `officer`
+--
 
 CREATE TABLE `officer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `oname` varchar(50) NOT NULL,
   `post` varchar(50) NOT NULL,
   `ostatus` varchar(10) DEFAULT NULL CHECK (`ostatus` in ('Active','Inactive')),
   `workStartTime` time NOT NULL,
-  `workEndTime` time NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+  `workEndTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `officer` */
+--
+-- Dumping data for table `officer`
+--
 
-insert  into `officer`(`id`,`oname`,`post`,`ostatus`,`workStartTime`,`workEndTime`) values 
-(9,'The Kubera','Manager','Active','08:00:00','17:00:00'),
-(10,'Test User','CEO','Inactive','11:11:00','22:10:00'),
-(11,'Manoj Sitoula','CEO','Active','10:16:00','15:18:00'),
-(12,'Test Twice','MA','Inactive','16:21:00','20:25:00');
+INSERT INTO `officer` (`id`, `oname`, `post`, `ostatus`, `workStartTime`, `workEndTime`) VALUES
+(9, 'The Kubera', 'Manager', 'Active', '08:00:00', '17:00:00'),
+(10, 'Test User', 'CEO', 'Inactive', '11:11:00', '22:10:00'),
+(11, 'Manoj Sitoula', 'CEO', 'Active', '10:16:00', '15:18:00'),
+(12, 'Test Twice', 'MA', 'Inactive', '16:21:00', '20:25:00');
 
-/*Table structure for table `visitor` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `visitor`;
+--
+-- Table structure for table `visitor`
+--
 
 CREATE TABLE `visitor` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `vname` varchar(50) NOT NULL,
   `mobile_no` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `vstatus` varchar(10) DEFAULT NULL CHECK (`vstatus` in ('Active','Inactive')),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `mobile_no` (`mobile_no`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  `vstatus` varchar(10) DEFAULT NULL CHECK (`vstatus` in ('Active','Inactive'))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `visitor` */
+--
+-- Dumping data for table `visitor`
+--
 
-insert  into `visitor`(`id`,`vname`,`mobile_no`,`email`,`vstatus`) values 
-(1,'New Visitor','9841123456','yourmail@kuber.com','Inactive'),
-(2,'Kamal Hassan','9823456843','guptori@hotmail.com','Active');
+INSERT INTO `visitor` (`id`, `vname`, `mobile_no`, `email`, `vstatus`) VALUES
+(1, 'New Visitor', '9841123456', 'yourmail@kuber.com', 'Inactive'),
+(2, 'Kamal Hassan', '9823456843', 'guptori@hotmail.com', 'Active');
 
-/*Table structure for table `work_days` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `work_days`;
+--
+-- Table structure for table `work_days`
+--
 
 CREATE TABLE `work_days` (
   `officer_id` int(11) NOT NULL,
-  `DAYOFWEEK` varchar(10) NOT NULL,
-  PRIMARY KEY (`officer_id`,`DAYOFWEEK`),
-  CONSTRAINT `work_days_ibfk_1` FOREIGN KEY (`officer_id`) REFERENCES `officer` (`id`)
+  `DAYOFWEEK` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `work_days` */
+--
+-- Dumping data for table `work_days`
+--
 
-insert  into `work_days`(`officer_id`,`DAYOFWEEK`) values 
-(9,'sunday'),
-(10,'friday'),
-(10,'monday'),
-(10,'sunday'),
-(11,'friday'),
-(11,'monday'),
-(11,'sunday'),
-(11,'wednesday'),
-(12,'monday'),
-(12,'saturday'),
-(12,'sunday'),
-(12,'thursday'),
-(12,'tuesday'),
-(12,'wednesday');
+INSERT INTO `work_days` (`officer_id`, `DAYOFWEEK`) VALUES
+(9, 'sunday'),
+(10, 'friday'),
+(10, 'monday'),
+(10, 'sunday'),
+(11, 'friday'),
+(11, 'monday'),
+(11, 'sunday'),
+(11, 'wednesday'),
+(12, 'monday'),
+(12, 'saturday'),
+(12, 'sunday'),
+(12, 'thursday'),
+(12, 'tuesday'),
+(12, 'wednesday');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `officer_id` (`officer_id`),
+  ADD KEY `visitor_id` (`visitor_id`);
+
+--
+-- Indexes for table `officer`
+--
+ALTER TABLE `officer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitor`
+--
+ALTER TABLE `visitor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `mobile_no` (`mobile_no`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `work_days`
+--
+ALTER TABLE `work_days`
+  ADD PRIMARY KEY (`officer_id`,`DAYOFWEEK`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `officer`
+--
+ALTER TABLE `officer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `visitor`
+--
+ALTER TABLE `visitor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activity`
+--
+ALTER TABLE `activity`
+  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`officer_id`) REFERENCES `officer` (`id`),
+  ADD CONSTRAINT `activity_ibfk_2` FOREIGN KEY (`visitor_id`) REFERENCES `visitor` (`id`);
+
+--
+-- Constraints for table `work_days`
+--
+ALTER TABLE `work_days`
+  ADD CONSTRAINT `work_days_ibfk_1` FOREIGN KEY (`officer_id`) REFERENCES `officer` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
